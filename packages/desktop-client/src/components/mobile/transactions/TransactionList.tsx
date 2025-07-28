8cb2e6acdfc3a3369b6afc7732676f260c829ab5
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-07-28 11:59:31 -0700

    Cleanup

--- packages/desktop-client/src/components/mobile/transactions/TransactionList.tsx
+++ packages/desktop-client/src/components/mobile/transactions/TransactionList.tsx
@@ -42,7 +42,7 @@ import {
   type TransactionEntity,
 } from 'loot-core/types/models';
 
-import { TransactionListItem } from './TransactionListItem';
+import { ROW_HEIGHT, TransactionListItem } from './TransactionListItem';
 
 import { FloatingActionBar } from '@desktop-client/components/mobile/FloatingActionBar';
 import { useScrollListener } from '@desktop-client/components/ScrollProvider';
@@ -237,7 +237,7 @@ export function TransactionList({
           aria-label={t('Loading more transactions...')}
           style={{
             // Same height as transaction list item
-            height: 60,
+            height: ROW_HEIGHT,
           }}
         />
       )}
